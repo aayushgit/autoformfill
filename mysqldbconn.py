7fbc30dc4040cd4f6afb500eb75f69861d590009
Aayush Sharma <sharmagiriaayush@gmail.com>
2018-05-21 16:29:54 +0545

    pymysql package used for DB connection

--- mysqldbconn.py
+++ mysqldbconn.py
@@ -1,51 +1,36 @@
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
-import mysql.connector
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+import pymysql
 
 
-def connect():
-    """ Connect to MySQL database """
-    try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='autoform fill',
-                                       user='root',
-                                       password='toor')
-        if conn.is_connected():
-            print('Connected to MySQL database')
+conn = pymysql.connect(host='localhost',
+                       database='autoformfill',
+                       user='root',
+                       password='root')
+if conn.is_connected():
+    print('Connected to MySQL database')
 
-    except Error as e:
-        print(e)
-
-    finally:
-        conn.close()
+cursor = conn.cursor()
 
 
 def insert_record(date, link, headline, content, newsof):
-    query = "INSERT INTO autofill(date,link,headline,content,newsof) VALUES(%s,%s,%s,%s,%s)"
-    args = (date, link, headline, content, newsof)
+    sql = """INSERT INTO autofill(date,link,headline,content,newsof)
+     VALUES(%s,%s,%s,%s,%s)"""
     try:
-        db_config = read_db_config()
-        conn = MySQLConnection(**db_config)
-
-        cursor = conn.cursor()
-        cursor.execute(query, args)
-
-        if cursor.lastrowid:
-            print('last insert id', cursor.lastrowid)
-        else:
-            print('last insert id not found')
-
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Commit your changes in the database
         conn.commit()
-    except Error as error:
-        print(error)
+    except:
+        # Rollback in case there is any error
+        conn.rollback()
 
-    finally:
-        cursor.close()
-        conn.close()
+
+# disconnect from server
+conn.close()
 
 
 def main():
-    connect()
     insert_record('2018/12/3', 'https://sharesansar.com/hello', 'This is my headline', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam quo dignissimos consequatur debitis ducimus dolorum voluptates, error at maiores odit eos reiciendis inventore ex, molestiae quibusdam rerum unde consequuntur doloremque!', 'NEPSE')
 
 
